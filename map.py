6a3de25875bd229a64ebb534450d929a44e8f940
Alinan1 <alinanesimiii@gmail.com>
2026-02-11 14:29:48 -0500

    Updated cipher mapping and improved encryption logic

--- map.py
+++ map.py
@@ -1,33 +1,38 @@
-import random
+import secrets
 import string
 
 chars = " " + string.punctuation + string.digits + string.ascii_letters
 chars = list(chars)
-keys = chars.copy()
 
-random.shuffle(keys)
+keys = chars.copy()
+secrets.SystemRandom().shuffle(keys)
 
 #print(f"chars: {chars}")
 #print(f"keys: {keys}")
 
+
+encrypt_map = dict(zip(chars, keys))
+decrypt_map = dict(zip(keys, chars))
+
 #Encryption 
-plaintext = input("Enter message to encrypt: ")
-cyphertext = "" 
+def encrypt(plain_text):
+    cipher_text = ""
+    for char in plain_text:
+        cipher_text += encrypt_map[char]
+    return cipher_text
 
-for letter in plaintext:
-    index = chars.index(letter)
-    cyphertext += keys[index]
+# Decryption
+def decrypt(cipher_text):
+    plain_text = ""
+    for char in cipher_text:
+        plain_text += decrypt_map[char]
+    return plain_text
 
-print(f"Original message: {plaintext}")
-print(f"Encrypted message: {cyphertext}")
+if __name__ == "__main__":
+    plain_text = input("Enter the plain text: ")
+    cipher_text = encrypt(plain_text)
+    decrypted_text = decrypt(cipher_text)
 
-#Decryption
-cyphertext = input("Enter message to Decrypt: ")
-plaintext = "" 
-
-for letter in cyphertext:
-    index = keys.index(letter)
-    plaintext += chars[index]
-
-print(f"Encrypted message: {cyphertext}")
-print(f"Original message: {plaintext}")
+    print(f"Plain Text: {plain_text}")
+    print(f"Cipher Text: {cipher_text}")
+    print(f"Decrypted Text: {decrypted_text}")
